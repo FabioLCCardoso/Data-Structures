907e699a3371111ad8fbc3d899a7b41c500d3e0e
FabioLCCardoso <fabioluiz.casanova@gmail.com>
2024-11-22 22:42:14 -0300

    initial commit

---
+++ Data-Structures/Exslides/ex1/ex1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+    //CODE FORMAT ACCORDING TO DOOM 3 (i guess)
+
+int main(){
+    //temp name is a temporary var to get the names.
+char tempname[20]{};
+char *name;
+//op is the operator var
+
+int op = 0;
+while(op >= 0 && op=<4){
+ if ( op > 4 || op < 0 ){
+    printf("Digite uma opcao valida");
+    op = 0;
+ }
+}
+
+
+
+
+return 0;
+}
---
+++ Data-Structures/Leetcode/twosum/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.*/
+
+
+
+// The answer is the twoSum function
+
+
+int* twoSum(int* nums, int numsSize, int target ,  int* returnSize ) {
+    int *indices = malloc(sizeof(int)* 2);
+    int i;
+
+    for(i = 0; i < numsSize; i++){
+        int j;
+    for (j = i + 1; j < numsSize; j++ ){
+        if( nums[i] + nums[j] == target ){
+            indices[0] = i;
+            indices[1] = j; 
+            *returnSize = 2;
+            return indices;
+            free(indices);
+        }
+    }
+    }
+
+    return NULL;
+}
+
+
+
+// this main doesnt work, the leetcode answer is the twoSum function
+/* int main() {
+  
+  int nums[7] = {0,1,1,1,1,1,3};
+  int* indices = twoSum(nums, 7, 5);
+  if (!indices) {
+    printf("no indices found.\n");
+    exit(1);
+  }
+  printf("The indices are: ");
+  int i;
+  for (i = 0; i < 2; i++){
+    printf("%i ", indices[i]);
+  }
+  printf("\n");
+}
+*/
