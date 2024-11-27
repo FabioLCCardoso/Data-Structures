bcdf0e7b4e1a4430056e8c5f20a7d97140145053
FabioLCCardoso <fabioluiz.casanova@gmail.com>
2024-11-26 21:03:41 -0300

    new leetcode exercise

---
+++ Data-Structures/Leetcode/palindromenum/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+//THIS CODE DOESN'T WORK ON ITS OWN, ITS JUST THE LEETCODE ANSWER
+
+//THIS CODE IS NOT OPTIMIZED YET, IT TAKES TIME IN SOME TESTS
+
+bool isPalindrome(int x) {
+    int tam = 100;
+    char str[tam];
+    int start = 0;
+    int end;   
+    char temp;
+    int y = x;
+
+    //converts x to a string
+
+    sprintf(str, "%d", x);
+
+    // loops the string from the end to its start, an then switch characters
+
+    end = strlen(str ) - 1;
+    while (start < end ){
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start ++;
+        end --;
+    }
+    
+    //converts the string back to an intenger and tests if its palindrome or not
+
+    sscanf(str, "%d", &x);   
+    if( x == y ){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
