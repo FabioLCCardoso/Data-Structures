5d2717d3f38c663f3d8bf203c863f388e55e6db7
FabioLCCardoso <fabioluiz.casanova@gmail.com>
2024-12-03 21:31:12 -0300

    new leetcode

--- Data-Structures/Leetcode/removeelement/main.c
+++ Data-Structures/Leetcode/removeelement/main.c
@@ -3,8 +3,9 @@
 int removeElement(int* nums, int numsSize, int val) {
     int k = 0;
 
-
+    // Loop to test and change the string
     for (int i = 0; i < numsSize; i++){
+    //Tests if the number is different to val, if it is, it skips to next intenger
         if( nums [i] != val){
             nums[k++] = nums[i]; 
         }
