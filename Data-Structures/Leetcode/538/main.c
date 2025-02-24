3398862f19f96c9a30979daa1202e621bb1fbbd4
FabioLCCardoso <fabioluiz.casanova@gmail.com>
2025-02-24 20:02:56 -0300

    new leetcode

--- Data-Structures/Leetcode/538/main.c
+++ Data-Structures/Leetcode/538/main.c
@@ -1,10 +1,12 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+    The left subtree of a node contains only nodes with keys less than the node's key.
+    The right subtree of a node contains only nodes with keys greater than the node's key.
+    Both the left and right subtrees must also be binary search trees.
+
  */
  /**
  * Definition for a binary tree node.
@@ -16,7 +18,7 @@
  */
 
  // Depth-First Search (DFS) function to convert BST to a Greater Sum Tree (GST).
-int dfs(struct TreeNode* root, int sum) {
+ int dfs(struct TreeNode* root, int sum) {
     if (root) {
         // Traverse the right subtree first (reverse in-order traversal).
         sum = dfs(root->right, sum);
