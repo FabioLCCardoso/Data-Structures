33b5091e71c833f6255493b0c131f70d03ae71e7
FabioLCCardoso <fabioluiz.casanova@gmail.com>
2025-03-05 20:19:41 -0300

    new leetcode

---
+++ Data-Structures/Leetcode/2236/main.c
@@ -0,0 +1,20 @@
+/**
+ * Root Equals Sum of Children
+
+ * 
+ * You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+ * Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+ * 
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+ bool checkTree(struct TreeNode* root) {
+    
+    return root->val == root->left->val + root->right->val;
+}
